865208fcce0e7ba2e2983cf3fd3c396e23f9555c
eotorresmolina <64936281+eotorresmolina@users.noreply.github.com>
2020-08-10 09:02:16 -0300

    Función ej4()
    Desarrollo y Finalización de la función ej4( )

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -100,11 +100,27 @@ def ej4():
     # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
     # e imprimir en pantalla el resultado de X (antes de incrementar) con print
 
+    while ((x < 10) and (x != 6)):
+
+        print('El Valor de la Variable "x" Antes de Incrementar es: {}'.format(x))
+        x += 2
+
     # Realice el mismo bucle "while" pero en vez de estar formado por una condición
     # compuesta, que el "while" siga iterando mientras <x sea menos a 10>, y dentro del
     # "while" consultar si <x es igual a 6>, y en ese caso realizar una interrupción del bucle
     # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
     # e imprimir en pantalla el resultado de X (antes de incrementar) con print
+
+    print('\n')
+    x = 0       # Vuelvo a Inicializar la Variable
+
+    while x < 10:
+        if x == 6:
+            break
+
+        print('El Valor de la Variable "x" Antes de Incrementar es: {} '.format(x))
+        x += 2
+
 
 
 def ej5():
@@ -148,7 +164,7 @@ if __name__ == '__main__':
     print("\nBienvenidos a otra clase de Inove con Python.\n\n")
     #ej1()
     #ej2()
-    ej3()
-    #ej4()
+    #ej3()
+    ej4()
     #ej5()
     #ej6()
