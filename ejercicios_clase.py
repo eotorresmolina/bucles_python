d5bfbc4cb65082d42a763f9fa6a94a8b5285b74b
eotorresmolina <64936281+eotorresmolina@users.noreply.github.com>
2020-08-10 21:48:59 -0300

    Programa Terminado
    Desarrollo y Finalización de todas las funciones correspondientes al programa.

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -11,7 +11,7 @@ Programa creado para poner a prueba los conocimientos
 adquiridos durante la clase
 '''
 
-__author__ = "Inove Coding School"
+__author__ = "Torres Molina Emmanuel"
 __email__ = "alumnos@inove.com.ar"
 __version__ = "1.2"
 
